a0fec154dece811d5147e09b171b1046cf8bfc57
Admin <admin@galben.org>
2025-01-26 11:24:49 +0100

    Implement pydantic on block

--- galbencoin/blockchain/block/block.py
+++ galbencoin/blockchain/block/block.py
@@ -1,20 +1,10 @@
-from enum import Enum
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
-class Block:
+class Block(BaseModel):
     id: int
-    hash: str
-    is_bigbang: bool
-    def __init__(self, id: int, hash:str=None, is_bigbang: bool = False):
-        """
-        None hash means it will be recalculated
-        """
-        if not isinstance(id, int):
-            raise TypeError(f"Expected 'id' to be 'int', got {type(id).__name__}")
-        if not isinstance(hash, str) or not isinstance(hash, None):
-            raise TypeError(f"Expected 'hash' to be 'str' or 'None', got {type(hash).__name__}")
-        if not isinstance(is_bigbang, bool):
-            raise TypeError(f"Expected 'is_bigbang' to be 'bool', got {type(is_bigbang).__name__}")
-        
-        self.id = id
-        self.hash = hash
-        self.is_bigbang = is_bigbang
+    timestamp: Optional[datetime]
+    previous_hash: Optional[str]
+    hash: Optional[str]
+    merkle_root: Optional[str]
